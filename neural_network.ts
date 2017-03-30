8e401a59d2bc12b680cc854e3c536ad7b56c2b3d
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-30 15:33:48 -0600

    add perceptrons

--- neural_network.ts
+++ neural_network.ts
@@ -35,3 +35,53 @@
 
     let activate = (result) => result > 0 ? 1 : -1;
 }
+
+// ---------------------------------------------------///
+
+{
+    const perceptron = (inputs: { weight, value }[], bias: number):(number) => number => {
+        const result = inputs.map((input) => {
+            return input.value * input.weight;
+        }).reduce((prev, curr) => {
+            return prev + curr;
+        }, bias);
+        
+        return (result) => result > 0 ? 1 : -1;
+    }
+}
+
+// --------------------------------------------------//
+
+{
+    class Perceptron {
+        weights: number[] = [];
+        learningConstnat: number = 0.01;
+
+        constructor(n: number) {
+            for (let i = 0; i < n; i++) {
+                this.weights[i] = (Math.random() * 2) - 1;
+            }
+        }
+
+        feedforward(inputs):number {
+            let sum:number = 0;
+            for (let i = 0; i < this.weights.length; i++) {
+                sum += inputs[i] * this.weights[i];
+            }
+            return this.activate(sum);
+        }
+
+        activate(sum) {
+            return sum > 0 ? 1 : -1;
+        }
+
+        train(inputs, desired) {
+            let guess:number = this.feedforward(inputs);
+            let error = desired - guess;
+            for (let i = 0; i < this.weights.length; i++) {
+                this.weights[i] += c * error * inputs[i];
+            }
+        }
+
+    }
+}
